e05b2aa761a8a15ef15abd8e5b7d84ec745618a1
Dorothy2020 <wangarraakoth@gmail.com>
2021-05-01 06:58:33 +0300

    tuples

---
+++ tupule.py
@@ -0,0 +1,13 @@
+# Range of indexes in tuples
+thistuple=("Kisumu","Nairobi","Nakuru","Bungoma","Nyeri","Busia","HomaBay")
+print(thistuple[2:5])
+
+# Unpacking Tuples
+person=("Techster",25,"Education")
+(Company,age,profile)=person
+print(Company)
+print(age)
+print(profile)
+
+
+
